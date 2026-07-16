782e727db7ecdb009b24d8aa7c6cf211e9e7780f
hjj.gemini <hjj.gemini@gmail.com>
2026-07-16 20:31:43 +0800

    feat(git): make domain boot registration atomic

--- apps/ezagent_domain_git/lib/ezagent_domain_git/application.ex
+++ apps/ezagent_domain_git/lib/ezagent_domain_git/application.ex
@@ -6,14 +6,35 @@ defmodule EzagentDomainGit.Application do
   @impl true
   def start(_type, _args) do
     with {:ok, _limits} <- Ezagent.DomainGit.ChangeLimits.current() do
-      Supervisor.start_link(
-        [
-          {Ezagent.DomainGit.AdapterRegistry, []},
-          Ezagent.DomainGit.TaskAccessSupervisor
-        ],
-        strategy: :one_for_one,
-        name: __MODULE__
-      )
+      boot_opts = Application.get_env(:ezagent_domain_git, :boot_registration, [])
+      later_children = Application.get_env(:ezagent_domain_git, :later_boot_children, [])
+
+      with {:ok, supervisor} <-
+             Supervisor.start_link(
+               [
+                 {Ezagent.DomainGit.AdapterRegistry, []},
+                 {Ezagent.DomainGit.BootRegistration, boot_opts},
+                 Ezagent.DomainGit.TaskAccessSupervisor
+               ],
+               strategy: :one_for_one,
+               name: __MODULE__
+             ) do
+        start_later_children(supervisor, later_children)
+      end
     end
+  end
+
+  defp start_later_children(supervisor, children) do
+    Enum.reduce_while(children, {:ok, supervisor}, fn child, {:ok, ^supervisor} ->
+      case Supervisor.start_child(supervisor, child) do
+        {:ok, _pid} ->
+          {:cont, {:ok, supervisor}}
+
+        {:error, reason} ->
+          :ok = Supervisor.terminate_child(supervisor, Ezagent.DomainGit.BootRegistration)
+          :ok = Supervisor.stop(supervisor)
+          {:halt, {:error, reason}}
+      end
+    end)
   end
 end
