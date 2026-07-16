9aac4eace55d20f6e92a0d157581800bdd9e2f7d
hjj.gemini <hjj.gemini@gmail.com>
2026-07-16 20:38:14 +0800

    fix(git): close boot rollback gaps

--- apps/ezagent_domain_git/lib/ezagent_domain_git/application.ex
+++ apps/ezagent_domain_git/lib/ezagent_domain_git/application.ex
@@ -7,24 +7,42 @@ defmodule EzagentDomainGit.Application do
   def start(_type, _args) do
     with {:ok, _limits} <- Ezagent.DomainGit.ChangeLimits.current() do
       boot_opts = Application.get_env(:ezagent_domain_git, :boot_registration, [])
+
+      task_access_child =
+        Application.get_env(
+          :ezagent_domain_git,
+          :task_access_child,
+          Ezagent.DomainGit.TaskAccessSupervisor
+        )
+
       later_children = Application.get_env(:ezagent_domain_git, :later_boot_children, [])
 
-      with {:ok, supervisor} <-
-             Supervisor.start_link(
-               [
-                 {Ezagent.DomainGit.AdapterRegistry, []},
-                 {Ezagent.DomainGit.BootRegistration, boot_opts},
-                 Ezagent.DomainGit.TaskAccessSupervisor
-               ],
-               strategy: :one_for_one,
-               name: __MODULE__
-             ) do
-        start_later_children(supervisor, later_children)
+      case Supervisor.start_link(
+             [{Ezagent.DomainGit.AdapterRegistry, []}],
+             strategy: :one_for_one,
+             name: __MODULE__
+           ) do
+        {:ok, supervisor} ->
+          start_registration(supervisor, boot_opts, [task_access_child | later_children])
+
+        {:error, _reason} = error ->
+          error
       end
     end
   end
 
-  defp start_later_children(supervisor, children) do
+  defp start_registration(supervisor, boot_opts, post_registration_children) do
+    case Supervisor.start_child(supervisor, {Ezagent.DomainGit.BootRegistration, boot_opts}) do
+      {:ok, _registration} ->
+        start_post_registration_children(supervisor, post_registration_children)
+
+      {:error, _reason} = error ->
+        :ok = Supervisor.stop(supervisor)
+        error
+    end
+  end
+
+  defp start_post_registration_children(supervisor, children) do
     Enum.reduce_while(children, {:ok, supervisor}, fn child, {:ok, ^supervisor} ->
       case Supervisor.start_child(supervisor, child) do
         {:ok, _pid} ->
