75d6ae4043142324b76754cbf4571251aa854259
zyli <lizhenyu0613@126.com>
2026-07-24 16:55:54 +0800

    test(world): extract display-name fixture

---
+++ apps/ezagent_plugin_world/test/support/agent_display_name_template.ex
@@ -0,0 +1,51 @@
+defmodule Ezagent.World.AgentDisplayNameTemplate do
+  @moduledoc false
+
+  @behaviour Ezagent.Kind.Template
+
+  @impl true
+  def template_name, do: "test.world_profile_display_name"
+
+  @impl true
+  def config_dir_namespace, do: "cc"
+
+  @impl true
+  def validate(_data), do: :ok
+
+  @impl true
+  def instantiate(_name, data, _workspace_uri) do
+    agent_uri = Ezagent.URI.new!(Map.fetch!(data, "agent_uri"))
+    config_dir = Map.fetch!(data, "allocated_config_dir")
+
+    respawn_template_data =
+      data
+      |> Map.put("agent_config_dir", config_dir)
+      |> Map.put("flavor", "world-profile-display-name")
+
+    case Ezagent.Kind.spawn(Ezagent.Entity.Agent, %{
+           uri: agent_uri,
+           config_dir_path: config_dir,
+           template_class: __MODULE__,
+           respawn_template_data: respawn_template_data
+         }) do
+      {:ok, _pid} ->
+        :ok = Ezagent.ReadyGate.put(agent_uri, :not_ready)
+
+        {:ok, [agent_uri],
+         %{
+           fresh?: true,
+           config_dir_path: config_dir,
+           respawn_template_data: respawn_template_data
+         }}
+
+      {:error, {:already_started, _pid}} ->
+        {:ok, [agent_uri], %{fresh?: false}}
+
+      {:error, {:already_registered, _}} ->
+        {:ok, [agent_uri], %{fresh?: false}}
+
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+end
